eeebff72884fce33f2d05e8a9a88c6b157d60c6b
B Mohan Reddy <mohanreddy@Mohans-MacBook-Pro.local>
2024-04-19 22:07:11 +0530

    -second commit

---
+++ src/main/scala/range_with_or.scala
@@ -0,0 +1,14 @@
+object range_with_or {
+
+  def main(args:Array[String]): Unit = {
+    val a = -15
+    if (a<(-10) || a>10) {
+      print("a in the range between -10, 10")
+    }
+    else{
+      print("a in NOT in range between -10,10")
+    }
+
+  }
+
+}
